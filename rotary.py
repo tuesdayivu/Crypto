60fe8cc1a5014fb8cf7128c75712e70f6d593506
Tuesday <40528233+tuesdayivu@users.noreply.github.com>
2018-07-13 17:59:25 -0700

    Add files via upload

--- rotary.py
+++ rotary.py
@@ -1,7 +1,44 @@
-import sys
+#!/usr/bin/env python
 
-action = sys.argv[1]
-string = sys.argv[2]
+__author__ = "TuesdayIVU (Kyle)"
+__credits__ = ["Irvine Underground", "Israel Torres", "Challenge Coin"]
+__license__ = "GPL"
+__version__ = "1.0"
+__maintainer__ = "TuesdayIVU (Kyle)"
+__email__ = "tuesdayivu@gmail.com"
+__status__ = "Meh"
+
+import argparse
+
+
+parser = argparse.ArgumentParser(
+	formatter_class=argparse.RawDescriptionHelpFormatter,
+	description=('''
+Decrypt or Encrypt Rotary Cipher Strings
+
+	  _...----..._
+       ,-'    ,-.     `-.
+     ,' ,-.  ( 4 )   ,-. `.
+   ,'  ( 5 )  `-'   ( 3 )  `.
+  / ,-. `-',-''  ``-.`-' ,-. \\
+ / ( 6 ) ,'          `. ( 2 ) \\
+:   `-' /    Rotary    \\ `-'   :
+| ,-.  :    ________    :  ,-. |
+|( 7 ) |   |__v1.0__|   | ( 1 )|
+| `-'  :                ;  `-' |
+:   ,-. \\    Cipher    /       ;
+ \\ ( 8 ) `.          ,'(`.    /
+  \\ `-' ,-.`-..__..-'   \\ `-./
+   `.  ( 9 )   ,-.       \\  ,'
+     `. `-'   ( 0 )      ,'`
+       `-._    `-'   _.-'
+           ```----\'\'\' 
+
+	'''))
+
+parser.add_argument('-d', metavar='encrypted_string', help='Decrypt a rotary cipher string')
+parser.add_argument('-e', metavar='decrypted_string', help='Encrypt a rotary cipher string')
+args = parser.parse_args()
 
 encrypt_dict = {
     'z': '0\\',
@@ -75,24 +112,20 @@ def rotary_encrypt(string):
 	return output
 
 
-if action == "-d":
-	#print "decrypt"
-	input_string = string.lower()
+if (args.d):
+	input_string = args.d.lower()
 	n = 2
 	# "explode" string into pairs of characters
 	string_list = [input_string[i:i+n] for i in range(0, len(input_string), n)]
 	decrypted_string = rotary_decrypt(string_list)
 	print decrypted_string.upper()
-
-elif action == "-e":
-	#print "encrypt"
-	input_string = string.lower()
+elif (args.e):
+	input_string = args.e.lower()
 	string_list = list(input_string)
 	encrypted_string = rotary_encrypt(string_list)
 	print encrypted_string.upper()
 else:
-	print "wat"
-	
+	parser.print_help()
 
 
 
